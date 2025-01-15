d342ec8b94dc5d1f7844bc70e7a914ecab6ba232
adith <adithkkadith@gmail.com>
2025-01-15 10:27:58 +0530

    project update

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from.import views
+from . import views
+
 urlpatterns = [
-       
+    path('', views.home, name='home'),  
+    path('login/', views.login_view, name='login'), 
+    path('products/', views.products_view, name='products'), 
+    path('contact/', views.contact_view, name='contact'),
 ]
+
+
