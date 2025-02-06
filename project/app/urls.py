e70d4329c514d1f03eef3231e7df2f20f90ed9f7
adith <adithkkadith@gmail.com>
2025-02-07 01:10:46 +0530

    t-shirt

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path
-from . import views
 
-urlpatterns = [
-    path('', views.home, name='home'),  
-    path('login/', views.login_view, name='login'), 
-    path('products/', views.products_view, name='products'), 
-    path('contact/', views.contact_view, name='contact'),
-]
+
+
+
+
+
+
 
 
