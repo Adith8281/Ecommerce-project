d650f425549f90fbdffc2d71017462461f16d387
adith <adithkkadith@gmail.com>
2025-03-03 17:23:53 +0530

    update 3/3/25

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,9 +1,15 @@
+from django.urls import path
+from .views import *
+from . import views
 
-
-
-
-
-
-
-
-
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('',  views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/',views.about, name='about'),
+    path('shop/',views.shop,name='shop'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('cart/',views.cart,name='cart')
+]
---
+++ project/app/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .form import CustomRegistrationForm, CustomLoginForm
+from .models import User
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate,logout
+from django.contrib import messages
+from django.contrib.auth.hashers import check_password
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomRegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+           
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "Username already taken.")
+                return render(request, 'register.html', {'form': form})
+
+            if User.objects.filter(email=email).exists():
+                messages.error(request, "Email is already registered.")
+                return render(request, 'register.html', {'form': form})
+
+          
+            hashed_password = make_password(password)
+
+            
+            User.objects.create(username=username, email=email, password=hashed_password)
+            messages.success(request, "Registration successful! You can now log in.")
+            return redirect('login')
+    else:
+        form = CustomRegistrationForm()
+
+    return render(request, 'register.html', {'form': form})
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = CustomLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                messages.error(request, "Invalid username or password.")
+                return render(request, 'login.html', {'form': form})
+
+           
+            if user and check_password(password, user.password):
+                request.session['user_id'] = user.id  
+                messages.success(request, "Login successful!")
+                return redirect('home')
+            else:
+                messages.error(request, "Invalid username or password.")
+    else:
+        form = CustomLoginForm()
+
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+
+
+def home(request):
+    return render(request,'home.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def shop(request):
+    return render(request,'shop.html')
+
+def checkout(request):
+    return render(request,'checkout.html')
+def cart(request):
+    return render(request,'cart.html')
